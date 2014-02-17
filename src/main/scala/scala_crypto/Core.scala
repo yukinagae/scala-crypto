a3664220ef0a923ee881a5584068baf0547dfbbc
Yuki Nagae <yuki.nagae1130@gmail.com>
2014-02-17 22:02:58 +0900

    何となく写経。

--- src/main/scala/scala_crypto/Core.scala
+++ src/main/scala/scala_crypto/Core.scala
@@ -4,6 +4,9 @@ import java.security.Security
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.KeyPairGenerator
 import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.Key
 
 object Core {
 
@@ -23,4 +26,44 @@ object Core {
     key_pair_generator.generateKeyPair
   }
 
+  def private_key(keyPair: KeyPair): PrivateKey = {
+    keyPair.getPrivate
+  }
+
+  def as_private_key(key: Key): PrivateKey = {
+    key match {
+      // TODO case k: KeyPair 
+      case k: PrivateKey => k
+      case _ => throw new RuntimeException(s"Dont't know how to convert to private key: ${key}")
+    }
+  }
+
+  def public_key(keyPair: KeyPair): PublicKey = {
+    keyPair.getPublic
+  }
+
+  def as_public_key(key: Key): PublicKey = {
+    key match {
+      // TODO case k: KeyPair
+      case k: PublicKey => k
+      case _ => throw new RuntimeException(s"Dont't know how to convert to public key: ${key}")
+    }
+  }
+
+  def algorithm(key: Key): String = {
+    key.getAlgorithm
+  }
+
+  def encoded(key: Key): Array[Byte] = {
+    key.getEncoded
+  }
+
+  def format(key: Key): String = {
+    key.getFormat
+  }
+
+  // TODO def create-cipher 
+  
+  
+  
 }
