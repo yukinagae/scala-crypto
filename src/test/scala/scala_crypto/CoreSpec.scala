c000f4461ba25463512a993415bf7c64748f3685
yukinagae <yuki.nagae1130@gmail.com>
2014-02-19 09:10:41 +0900

    テストコードをコピー。（public_keyとprivate_keyの同一チェックは、Scalaでは単純に==でチェック出来るので楽）

--- src/test/scala/scala_crypto/CoreSpec.scala
+++ src/test/scala/scala_crypto/CoreSpec.scala
@@ -6,9 +6,10 @@ import java.security.Security
 class CoreSpec extends Specification {
 
   "scala-crypto" should {
-    "test-encrypt-decrypt" in {
 
-      import Core._
+    import Core._
+
+    "test-encrypt-decrypt" in {
 
       val key_pair = generate_key_pair
       val data = "secret text"
@@ -35,6 +36,68 @@ class CoreSpec extends Specification {
       println(decrypted_text)
 
       decrypted_text mustEqual data
+    }
+
+    "basic-password-protection" in {
+      val password = "password"
+      val salt = 2079324
+      val algorithm = default_encrypt_password_algorithm
+      val n = default_encrypt_password_n
+      val encrypted_password = encrypt_password_string(password, salt, algorithm, n)
+
+      println(s"password:${password}")
+      println(s"encrypted_password:${encrypted_password}")
+
+      encrypted_password mustNotEqual password
+      encrypted_password mustEqual encrypt_password_string(password, salt, algorithm, n)
+      encrypted_password mustNotEqual encrypt_password_string(password, salt, algorithm, 1)
+    }
+
+    "save-load-key-pairs" in {
+
+      val key_pair = generate_key_pair
+      val key_pair_map = get_key_pair_map(key_pair)
+
+      println(s"key_pair_map:${key_pair_map}")
+
+      val public_key = key_pair_map._1
+      val private_key = key_pair_map._2
+
+      public_key._1 mustEqual default_algorithm
+      private_key._1 mustEqual default_algorithm
+
+      println(s"public_key :bytes${public_key._2}")
+      println(s"private_key :bytes${private_key._2}")
+
+      val decoded_key_pair = decode_key_pair(key_pair_map)
+
+      println(s"key_pair:${key_pair}")
+      println(s"decoded_key_pair:${decoded_key_pair}")
+      println(s"key_pair public:${key_pair.getPublic}")
+      println(s"decoded_key_pair public:${decoded_key_pair.getPublic}")
+      println(s"key_pair private:${key_pair.getPrivate}")
+      println(s"decoded_key_pair private:${decoded_key_pair.getPrivate}")
+
+      println(key_pair.getPublic == decoded_key_pair.getPublic)
+      println(key_pair.getPrivate == decoded_key_pair.getPrivate)
+
+      key_pair.getPublic mustEqual decoded_key_pair.getPublic
+      key_pair.getPrivate mustEqual decoded_key_pair.getPrivate
+    }
+
+    "signature" in {
+
+      val test_data = "Test data to sign"
+      val key_pair = generate_key_pair
+      val signature = sign(key_pair, test_data)
+
+      println(signature)
+
+      verify_signature(key_pair, test_data, signature) must beTrue
+
+      verify_signature(generate_key_pair, test_data, signature) must beFalse
+
+      success
     }
   }
 
